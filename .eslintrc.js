be219e1f000512d37ea203fdbd1380d0b8f5ac2b
zhangran <zhangran963@gmail.com>
2022-02-01 19:11:39 +0800

    feat: 格式化

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,28 +4,44 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
   ],
   parserOptions: {
     ecmaVersion: 2020,
+    // ecmaFeatures: {
+    //   jsx: false,
+    //   modules: true,
+    // },
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: ['error', 'single'],
+    /* 合并prettier的配置 */
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        // parser: 'vue',
+        semi: false,
+      },
+    ],
+    /* data函数, 不需要定义格式 */
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
         jest: true,
       },
     },
   ],
-};
+}
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -119,7 +119,7 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
+import { Options, Vue } from 'vue-class-component'
 
 @Options({
   props: {
@@ -127,7 +127,7 @@ import { Options, Vue } from "vue-class-component";
   },
 })
 export default class HelloWorld extends Vue {
-  msg!: string;
+  msg!: string
 }
 </script>
 
